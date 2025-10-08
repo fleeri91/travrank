544e4c102dc15b0d36d756422501fd728ef732f1
Flemming Eriksson <fleeri91@gmail.com>
2025-10-08 08:28:13 +0200

    theme + provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+
+import '@radix-ui/themes/styles.css'
 import './globals.css'
+
+import { SWRProvider } from '@/providers/SWRProvider'
+import { Theme } from '@radix-ui/themes'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +32,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex justify-center">
+          <Theme
+            appearance="dark"
+            accentColor="blue"
+            grayColor="gray"
+            panelBackground="solid"
+            className="w-full"
+          >
+            <SWRProvider>{children}</SWRProvider>
+          </Theme>
+        </main>
       </body>
     </html>
   )
